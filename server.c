f2ee45eb3a20d69aa1e0b8122cdcadbd8ef2eaa6
Christina O'Donnell <cdo@mutix.org>
2024-01-30 21:34:27 +0000

    Flesh out server, switching to select looping

--- server.c
+++ server.c
@@ -1,12 +1,15 @@
 #include "util.h"
 #include "server.h"
+#include "message.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int server_init(server_t *server) {
   memset(server, 0, sizeof(server_t));
+  FD_ZERO(&server->fds);
 
   if (read_port("Enter a port number: ", &server->port) < 0) {
     goto error_cleanup;
@@ -19,9 +22,39 @@ int server_init(server_t *server) {
   else
     printf("Socket bound and listening...\n");
 
+  FD_SET(server->socket, &server->fds);
+  server->max_fd = server->socket;
   return 0;
 
 error_cleanup:
+  return -1;
+}
+
+int server_handle_connect(server_t *server, SOCKET new_socket, struct in_addr addr) {
+  int i;
+  for (i = 0; i < MAX_CONNECTIONS; i++) {
+    // socket == 0 means the connection slot is free.
+    if (server->connections[i].socket == 0)
+      break;
+  }
+  if (i == MAX_CONNECTIONS) {
+    send_format(new_socket, "No more room on this server, sorry.");
+    close(new_socket);
+    return -1;
+  }
+  if (new_socket > server->max_fd)
+    server->max_fd = new_socket;
+  // We're accepting this connection. Add it to our list
+  server->connections[i].socket = new_socket;
+  server->connections[i].addr = addr;
+  server->connections[i].user_id = NULL;
+  server->connections[i].room = NULL;
+  FD_SET(new_socket, &server->fds);
+  return 0;
+}
+
+int server_process_message(server_t *server, SOCKET socket) {
+  //TODO
   return -1;
 }
 
@@ -29,6 +62,33 @@ void server_run() {
   static server_t server = {0};
   if (server_init(&server) < 0)
     goto abort;
+
+
+  while (1) {
+    fd_set read_fds = server.fds;
+    if (select(server.max_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
+      perror("Error select failed");
+      goto abort;
+    }
+    for (int i = 0; i <= server.max_fd; i++) {
+      if (FD_ISSET(i, &read_fds)) {
+        if (i == server.socket) {
+          // New connection
+          struct in_addr addr;
+          SOCKET new_socket = accept_connection(server.socket, &addr);
+          if (new_socket < 0)
+            goto abort;
+          if (server_handle_connect(&server, new_socket, addr) < 0) {
+            goto abort;
+          }
+        } else {
+          if (server_process_message(&server, i) < 0)
+            goto abort;
+        }
+      }
+    }
+  }
+
   struct in_addr addr = {0};
   int connected_socket = accept_connection(server.socket, &addr);
   if (connected_socket < 0)
--- server.h
+++ server.h
@@ -1,15 +1,42 @@
 #include "socket.h"
 
-typedef struct connection_t {
+// Maximum total connections, users, rooms that the server can track.
+#define MAX_CONNECTIONS 20
+#define MAX_USERS 100
+#define MAX_ROOMS 10
+#define MAX_USERS_PER_ROOM 10
+
+typedef struct connection_t connection_t;
+typedef struct user_t user_t;
+typedef struct room_t room_t;
+typedef struct server_t server_t;
+
+struct connection_t {
   SOCKET socket;
   struct in_addr addr;
-  struct connection_t *next;
-} connection_t;
+  user_t *user_id;
+  room_t *room;
+};
 
-typedef struct server_t {
+struct user_t {
+  char *username;
+  char *password;
+  connection_t *connection;
+};
+
+struct room_t {
+  char *name;
+  connection_t *connected[MAX_USERS_PER_ROOM];
+};
+
+struct server_t {
   SOCKET socket;
   short port;
-  connection_t *connection_list;
-} server_t;
+  fd_set fds;
+  SOCKET max_fd;
+  connection_t connections[MAX_CONNECTIONS];
+  user_t users[MAX_USERS];
+  room_t rooms[MAX_ROOMS];
+};
 
 void server_run();
