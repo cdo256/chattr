16a5776d12c2ee0d186b551b3328ceee1663a817
Christina O'Donnell <cdo@mutix.org>
2024-01-31 15:13:17 +0000

    Start at windows compatibility

--- windows_socket.c
+++ windows_socket.c
@@ -1,40 +1,26 @@
-#ifdef __unix__
+#ifdef _WIN32
 #include "util.h"
 #include "socket.h"
-#include <arpa/inet.h>
-#include <errno.h>
-#include <net/if.h>
-#include <netinet/ip.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
 int set_socket_nonblocking(SOCKET sock) {
-  printf("Setting socket %d as non-blocking\n", sock);
-  int flags = fcntl(sock, F_GETFL, 0);
-  if (flags < 0) {
-    close(sock);
-    perror("Error: Unable to get socket properties");
-    return -1;
-  }
-  fcntl(sock, F_SETFL, flags | O_NONBLOCK);
-  if (flags < 0) {
-    close(sock);
+  printf("Setting socket %llu as non-blocking\n", (unsigned long long)sock);
+  u_long mode = 1; // 1 to enable non-blocking socket
+  if (ioctlsocket(sock, FIONBIO, &mode) != 0) {
+    closesocket(sock);
     perror("Error: Unable to set socket as non-blocking");
     return -1;
   }
-
   return 0;
 }
 
 SOCKET make_client_socket(const char *ip_addr, int port) {
   SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (sock < 0) {
+  if (sock == INVALID_SOCKET) {
     perror("Error: Couldn't make socket");
     return -1;
   }
@@ -44,8 +30,8 @@ SOCKET make_client_socket(const char *ip_addr, int port) {
   addr.sin_addr.s_addr = inet_addr(ip_addr);
   addr.sin_port = htons(port);
   int connect_result = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-  if (connect_result < 0 && errno != EINTR) {
-    close(sock);
+  if (connect_result == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK) {
+    closesocket(sock);
     perror("Error: Unable to connect");
     return -1;
   }
@@ -58,7 +44,7 @@ SOCKET make_client_socket(const char *ip_addr, int port) {
 
 SOCKET make_server_socket(int port) {
   SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (sock < 0) {
+  if (sock == INVALID_SOCKET) {
     perror("Server: Couldn't make socket");
     return -1;
   }
@@ -67,15 +53,15 @@ SOCKET make_server_socket(int port) {
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
   addr.sin_port = htons(port);
   int bind_result = bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-  if (bind_result < 0) {
-    close(sock);
+  if (bind_result == SOCKET_ERROR) {
+    closesocket(sock);
     perror("Server: Couldn't bind to port");
     return -1;
   }
   // Hardcode backlog of 20.
   int listen_result = listen(sock, 20);
-  if (listen_result < 0) {
-    close(sock);
+  if (listen_result == SOCKET_ERROR) {
+    closesocket(sock);
     perror("Server: Couldn't listen to port");
     return -1;
   }
@@ -88,11 +74,10 @@ SOCKET make_server_socket(int port) {
 
 SOCKET accept_connection(SOCKET sock, struct in_addr *client_addr) {
   struct sockaddr_in addr = {0};
-  socklen_t addrlen = sizeof(addr);
+  int addrlen = sizeof(addr);
   SOCKET new_sock = accept(sock, (struct sockaddr *)&addr, &addrlen);
-  if (new_sock < 0) {
-    close(sock);
-    perror("Server: Couldn't listen to port");
+  if (new_sock == INVALID_SOCKET) {
+    perror("Server: Couldn't accept connection");
     return -1;
   }
   memcpy(client_addr, &addr.sin_addr, sizeof(struct in_addr));
@@ -108,7 +93,7 @@ int send_string(SOCKET sock, char *string) {
   char *buffer = malloc(2 + length);
   if (buffer == NULL) {
     fprintf(stderr, "Unable to allocate buffer to send string. Aborting..\n");
-    close(sock);
+    closesocket(sock);
     return -1;
   }
   // Store the length first
@@ -116,10 +101,10 @@ int send_string(SOCKET sock, char *string) {
   // Then the string
   memcpy(buffer + 2, string, length);
   int send_result = send(sock, buffer, length + 2, 0);
-  if (send_result < 0) {
+  if (send_result == SOCKET_ERROR) {
     perror("Unable to send message");
     free(buffer);
-    close(sock);
+    closesocket(sock);
     return -1;
   }
   free(buffer);
@@ -129,33 +114,33 @@ int send_string(SOCKET sock, char *string) {
 int recv_message(SOCKET sock, char **buffer) {
   unsigned short length;
   int recv_result;
-  recv_result = recv(sock, &length, 2, MSG_WAITALL);
+  recv_result = recv(sock, (char *)&length, 2, MSG_WAITALL);
 
-  if (recv_result < 0) {
-    if (errno == EAGAIN) {
+  if (recv_result == SOCKET_ERROR) {
+    if (WSAGetLastError() == WSAEWOULDBLOCK) {
       // Not a real error code. Ignore
       return 0;
     }
     perror("Error: Unable to recv message length");
-    close(sock);
+    closesocket(sock);
     return -1;
   } else if (recv_result == 0) {
-    return 0;
+    return 0; // Connection has been gracefully closed
   } else if (recv_result < 2) {
-    eprintf("Incomplete packet. Only %d/%d bytes recieved\n", recv_result, 2);
-    close(sock);
+    fprintf(stderr, "Incomplete packet. Only %d/%d bytes received\n", recv_result, 2);
+    closesocket(sock);
     return -1;
   }
   length = ntohs(length);
-  *buffer = resize(*buffer, length + 1);
+  *buffer = (char *)realloc(*buffer, length + 1);
   recv_result = recv(sock, *buffer, length, MSG_WAITALL);
-  if (recv_result < 0) {
+  if (recv_result == SOCKET_ERROR) {
     perror("Unable to recv message body");
-    close(sock);
+    closesocket(sock);
     return -1;
   } else if (recv_result < length) {
-    eprintf("Incomplete packet. Only %d/%d bytes recieved\n", recv_result + 2, length + 2);
-    close(sock);
+    fprintf(stderr, "Incomplete packet. Only %d/%d bytes received\n", recv_result + 2, length + 2);
+    closesocket(sock);
     return -1;
   }
   (*buffer)[length] = '\0';
